955e37097e07f8cfaf08d901ceb0bfe593f6dea8
Thales Ludwig <thales.ludwig@gmail.com>
2018-06-14 18:42:02 -0300

    Adicionado controller de geração de views. #4

---
+++ app/Http/Controllers/ViewController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ViewController extends Controller
+{
+    public function generateViewInsert(){
+        /**
+         * TODO:
+         * navegar até a pasta src;
+         * criar pasta do componente;
+         * criar arquivos html, ts, css;
+         * informar excedente ao usuário;
+         */ 
+    }
+
+    public function generateViewUpdate(){
+        
+    }
+
+    public function generateViewList(){
+        
+    }
+
+    public function generateViewDashboard(){
+        
+    }
+}
